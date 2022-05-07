a2b2375daa8b5089aa9a5e5aa012e3d6254da603
GeorgyJavajun <103282860+GeorgyJavajun@users.noreply.github.com>
2022-05-07 17:58:45 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,11 @@ public class Main {
     public static void main(String[] args) {
            Scanner sc = new Scanner(System.in);
         
+             while(true) {
               String input = sc.nextLine();
               System.out.println(calc(input));
-           
+             if(input.equals("exit")) { break; }
+             }
     }
 
 
