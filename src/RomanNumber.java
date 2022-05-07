e02e054d40f12810a8bc050d5a50cbc7a4ab0bb3
GeorgyJavajun <103282860+GeorgyJavajun@users.noreply.github.com>
2022-05-07 13:28:48 +0300

    Update RomanNumber.java

--- src/RomanNumber.java
+++ src/RomanNumber.java
@@ -99,6 +99,7 @@ public enum RomanNumber {
     XCVIII("XCVIII",98),
     XCIX("XCIX",99),
     C("C", 100);
+    //Roman number i'm copy from internet, and next use hotkey(ctrl + ctrl + down). Then just write and write and write) 
 
 
 private int value;
