d4df1637857fc071a1fca07fedc4ff1d6c95bb38
GeorgyJavajun <tomatoesjuice@mail.ru>
2022-05-07 13:14:42 +0300

    Arabic\Roman calculator

---
+++ src/Main.java
@@ -0,0 +1,51 @@
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+           Scanner sc = new Scanner(System.in);
+           String input = sc.nextLine();
+
+        System.out.println(calc(input));
+    }
+
+
+    
+
+    public static String calc(String input) throws ArithmeticException {
+        String[] splitInput = input.split(" ");
+        String arithmeticChar = splitInput[1];
+
+
+        try {
+            int firstNum = Integer.parseInt(splitInput[0]);
+            int secNum = Integer.parseInt(splitInput[2]);
+
+            if (firstNum > 0 && firstNum < 11 && secNum > 0 && secNum < 11){
+                switch (arithmeticChar) {
+                    case "+" -> { return String.valueOf(firstNum + secNum); }
+                    case "-" -> { return String.valueOf(firstNum - secNum); }
+                    case "*" -> { return String.valueOf(firstNum * secNum); }
+                    default  -> { return String.valueOf(firstNum / secNum); }
+                }
+            }else { throw new ArithmeticException(); }
+
+        }catch (NumberFormatException e) {
+            int firstNum = RomanNumber.toInteger(splitInput[0]);
+            int secNum = RomanNumber.toInteger(splitInput[2]);
+
+            if (firstNum > 0 && firstNum < 11 && secNum > 0 && secNum < 11){
+                switch (arithmeticChar) {
+                    case "+" -> { return RomanNumber.toString(firstNum + secNum); }
+                    case "-" -> { return RomanNumber.toString(firstNum - secNum); }
+                    case "*" -> { return RomanNumber.toString(firstNum * secNum); }
+                    default  -> { return RomanNumber.toString(firstNum / secNum); }
+                }
+            }else  { throw new ArithmeticException(); }
+        }
+
+    }
+
+}
